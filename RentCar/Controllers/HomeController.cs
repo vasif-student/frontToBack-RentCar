1f8193dac6b9fe04027dd50904ba33dc86c43851
vasif-student <vasifah@code.edu.az>
2022-03-24 23:57:23 +0400

    frontToBack

---
+++ RentCar/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentCar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RentCar/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCar.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentCar.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Logo> Logos { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<PartnerInfoCard> PartnerInfoCards { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
